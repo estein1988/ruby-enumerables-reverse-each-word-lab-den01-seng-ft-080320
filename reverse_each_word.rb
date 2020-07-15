05e6bed20a6a661992aec06bd98d1815c0f975df
Elliott Stein <estein1988@gmail.com>
2020-07-15 16:00:00 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(sentence)
+  array = string.split(" ")
+  return_array = []
+  array.each do |sentence|
+    return_array << sentence.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word (sentence)
+  array = sentence.split(" ")
+  return_array = []
+  array.collect do |sentence|
+    return_array << sentence.reverse
+  end
+  return_array.join(" ")
+end
