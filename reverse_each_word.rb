a719c0294ee3db03dd33c5a26ccf29fe440c063c
Elliott Stein <estein1988@gmail.com>
2020-07-15 15:57:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@ def reverse_each_word (sentence)
   array = sentence.split(" ")
   return_array = []
   array.collect do |sentence|
-    "#{sentence}".reverse
+    return_array << sentence.reverse
   end
   return_array.join(" ")
 end
